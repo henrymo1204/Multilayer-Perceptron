2233832201f256a4b9ea0f96909e7a21b80011be
henrymo1204 <48542636+henrymo1204@users.noreply.github.com>
2020-12-11 01:40:45 -0800

    Add files via upload

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,6 +4,8 @@ import math
 
 
 def sigmoid(x):
+    if x < 0:
+        return 1 - 1 / (1 + math.exp(x))
     return 1 / (1 + math.exp(-x))
 
 
@@ -11,11 +13,30 @@ def derivative_sigmoid(x):
     return x * (1 - x)
 
 
+def num_to_list(x):
+    if x == 0:
+        return [1, 0, 0, 0, 0, 0, 0, 0]
+    elif x == 1:
+        return [0, 1, 0, 0, 0, 0, 0, 0]
+    elif x == 2:
+        return [0, 0, 1, 0, 0, 0, 0, 0]
+    elif x == 3:
+        return [0, 0, 0, 1, 0, 0, 0, 0]
+    elif x == 4:
+        return [0, 0, 0, 0, 1, 0, 0, 0]
+    elif x == 5:
+        return [0, 0, 0, 0, 0, 1, 0, 0]
+    elif x == 6:
+        return [0, 0, 0, 0, 0, 0, 1, 0]
+    else:
+        return [0, 0, 0, 0, 0, 0, 0, 1]
+
+
 class NeuralNetwork:
     def __init__(self):  # constructor
-        self.inputNodes = 2
-        self.hiddenNodes = 2
-        self.outputNodes = 1
+        self.inputNodes = 10
+        self.hiddenNodes = 10
+        self.outputNodes = 8
         self.weights_input_to_hidden = [[random.choice([-1, 1]) for x in range(self.inputNodes + 1)] for y in
                                         range(self.hiddenNodes + 1)]
         self.weights_hidden_to_output = [[random.choice([-1, 1]) for x in range(self.hiddenNodes + 2)] for y in
@@ -23,45 +44,47 @@ class NeuralNetwork:
         self.hidden_bias = 1
         self.output_bias = 1
 
-    def train(self, vector, target):  # feed forward algorithm
+    def train(self, vector, label):  # feed forward algorithm
         sigmoid_function = np.vectorize(sigmoid)  # sigmoid function to apply to the matrix
         derivative_sigmoid_function = np.vectorize(derivative_sigmoid)
 
-        vector += [self.hidden_bias]
+        test = vector.copy()
+        target = num_to_list(label)
+        test += [self.hidden_bias]
         # feed forward from input to hidden
-        inputs = np.array(vector)
+        inputs = np.array(test)
         # change inputs from horizontal form to vertical form
-        inputs = inputs.reshape(3, 1)
-        print('input vector: ', inputs)
+        inputs = inputs.reshape(11, 1)
+        #print('input vector: ', inputs)
         hidden_weights = np.array(self.weights_input_to_hidden)
-        print('hidden weights: ', hidden_weights)
+        #print('hidden weights: ', hidden_weights)
         hidden_result = hidden_weights.dot(inputs)
-        print('hidden results: ', hidden_result)
+        #print('hidden results: ', hidden_result)
         hidden_result = sigmoid_function(hidden_result)
-        print('hidden result after applying sigmoid: ', hidden_result)
+        # print('hidden result after applying sigmoid: ', hidden_result)
         hidden_result = np.append(hidden_result, [self.output_bias])
-        print('hidden result with output bias: ', hidden_result)
+        # print('hidden result with output bias: ', hidden_result)
 
         # feed forward from hidden to output
         output_weights = np.array(self.weights_hidden_to_output)
-        print('output weights: ', output_weights)
+        #print('output weights: ', output_weights)
         output_result = output_weights.dot(hidden_result)
-        print('output result: ', output_weights)
+        #print('output result: ', output_weights)
         result = sigmoid_function(output_result)
-        print('final result: ', result[0])
-        guess = result[0]
+        #print('final result: ', result)
+        guess = result
 
         # output error
         output_error = target - guess
 
         # output gradient
         guess = derivative_sigmoid_function(guess)
-        output_gradient = guess * output_error
+        output_gradient = guess.dot(output_error)
         output_gradient *= 0.1
 
         # hidden to output deltas
         transposed_hidden = np.transpose(hidden_result)
-        weights_hidden_to_output_deltas = output_gradient * transposed_hidden
+        weights_hidden_to_output_deltas = transposed_hidden * output_gradient
         # print(weights_hidden_to_output_deltas)
         # print(self.weights_hidden_to_output)
         current_weights_hidden_to_output = np.array(self.weights_hidden_to_output)
@@ -72,7 +95,7 @@ class NeuralNetwork:
         # hidden error
         transposed_input = np.transpose(self.weights_hidden_to_output)
         hidden_error = transposed_input.dot(output_error)
-        print(hidden_error)
+        #print(hidden_error)
 
         # hidden gradient
         hidden_gradient = derivative_sigmoid_function(hidden_result)
@@ -81,13 +104,38 @@ class NeuralNetwork:
 
         # input to hidden deltas
         transposed_input = np.transpose(inputs)
-        weights_input_to_hidden_deltas = hidden_gradient.dot(transposed_input)
+        weights_input_to_hidden_deltas = transposed_input * hidden_gradient
         current_weights_input_to_hidden = np.array(self.weights_input_to_hidden)
         deltas_weights_input_to_hidden = np.array(np.transpose(weights_input_to_hidden_deltas))
         self.weights_input_to_hidden = np.add(current_weights_input_to_hidden, deltas_weights_input_to_hidden)
-        print(self.weights_input_to_hidden)
 
-    # def test(self):  # testing algorithm
+    def test(self, vector):  # testing algorithm
+        sigmoid_function = np.vectorize(sigmoid)  # sigmoid function to apply to the matrix
+
+        test = vector.copy()
+        test += [self.hidden_bias]
+        # feed forward from input to hidden
+        inputs = np.array(test)
+        # change inputs from horizontal form to vertical form
+        inputs = inputs.reshape(11, 1)
+        # print('input vector: ', inputs)
+        hidden_weights = np.array(self.weights_input_to_hidden)
+        # print('hidden weights: ', hidden_weights)
+        hidden_result = hidden_weights.dot(inputs)
+        # print('hidden results: ', hidden_result)
+        hidden_result = sigmoid_function(hidden_result)
+        # print('hidden result after applying sigmoid: ', hidden_result)
+        hidden_result = np.append(hidden_result, [self.output_bias])
+        # print('hidden result with output bias: ', hidden_result)
+
+        # feed forward from hidden to output
+        output_weights = np.array(self.weights_hidden_to_output)
+        # print('output weights: ', output_weights)
+        output_result = output_weights.dot(hidden_result)
+        # print('output result: ', output_result)
+        result = sigmoid_function(output_result)
+        # print('final result: ', result)
+        return result
 
 #  temp = NeuralNetwork()
 #  v = [1, 2]
